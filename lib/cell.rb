baa133309108dc354fc90e930f30e2a1683063a0
シュール <shule517@gmail.com>
2017-07-17 23:40:08 +0900

    Cell#nextを実装

--- lib/cell.rb
+++ lib/cell.rb
@@ -14,4 +14,12 @@ class Cell
   def born
     @alive = true
   end
+
+  def next(arround_alive_count)
+    if alive?
+      die if arround_alive_count <= 1 || 4 <= arround_alive_count
+    else
+      born if arround_alive_count == 3
+    end
+  end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -30,4 +30,50 @@ describe Cell do
       expect(dead_cell.alive?).to eq true
     end
   end
+
+  describe '#next' do
+    context '生きている場合' do
+      let(:alive_cell) { Cell.new(true) }
+      [
+        [0, false], # 過疎
+        [1, false], # 過疎
+        [2, true],
+        [3, true],
+        [4, false], # 過密
+        [5, false], # 過密
+        [6, false], # 過密
+        [7, false], # 過密
+        [8, false], # 過密
+      ].each do |arround_alive_count, alive|
+        context "周りに生きているセルが#{arround_alive_count}個いる場合" do
+          it "#{alive ? '生きていること' : '死んでいること'}" do
+            alive_cell.next(arround_alive_count)
+            expect(alive_cell.alive?).to eq alive
+          end
+        end
+      end
+    end
+
+    context '死んでいる場合' do
+      let(:dead_cell) { Cell.new(false) }
+      [
+        [0, false],
+        [1, false],
+        [2, false],
+        [3, true], # 生き返る
+        [4, false],
+        [5, false],
+        [6, false],
+        [7, false],
+        [8, false],
+      ].each do |arround_alive_count, alive|
+        context "周りに生きているセルが#{arround_alive_count}個いる場合" do
+          it "#{alive ? '生きていること' : '死んでいること'}" do
+            dead_cell.next(arround_alive_count)
+            expect(dead_cell.alive?).to eq alive
+          end
+        end
+      end
+    end
+  end
 end
